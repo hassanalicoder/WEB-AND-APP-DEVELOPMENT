4aeb8723472334bed02b7bb2a7291f01cd44c826
hassanalicoder <hassan8284262@gmail.com>
2025-10-22 02:57:39 +0500

    Add files via upload

---
+++ java script assigment1/script,.js
@@ -0,0 +1,34 @@
+alert("error !please enter a valid password");
+alert("Welcome to js land \n Happy coding")
+alert("Welcome to JS ")
+alert("Happy Coding! \n prevent this page from additional dialogue")
+
+console.log("Welcome to JS Land...\nHappy Coding!");
+
+var username=("hassancoder")
+var myname=("HELLOWORLD")
+alert(username)
+alert(myname)
+
+var student=("tayyab")
+var age=("25 years")
+var achievement=("full stack developer")
+alert(student)
+alert(age)
+alert(achievement)
+
+var pizza=("pizza \n pizz \n piz \n pi")
+alert(pizza)
+
+var email=("hassan8284262@gmail.com")
+alert(email)
+
+document.write("Hello, i write this using JAVASCRIPT<br>");
+
+document.write("my birthyear is 2010<br>");
+var visits=("you visited this website for 18 times")
+
+
+
+
+document.write("MUHAMMAD AHSAN ordered 5 phones from daraz online shopping<br>");
